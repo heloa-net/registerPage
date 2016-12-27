975c33e75b43f0e75266c6cc818578c4bb238183
Heloá Meneses <heloa.meneses@gmail.com>
2016-12-27 10:20:19 -0200

    Removing mongoDB

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 var express = require('express');
 var app = express();
-var mongojs = require('mongojs');
-var db = mongojs('users', ['users']);
 
 app.use(express.static(__dirname + "/public"));
 
-// app.get('/users', function (req, res) {
-//     console.log("I received a GET request");
+app.get('/users', function (req, res) {
+    console.log("I received a GET request");
+});
 
-//     db.users.find(function (err, docs) {
-//         console.log(docs);
-//         res.json(docs);
-//     });
-// });
+app.post('/users', function (req, res) {
+    console.log(res.body + ' OII');
 
-// app.post('/users', function (req, res) {
-//     console.log(res.body);
-// });
+});
 
 app.listen(3000);
 console.log("Servidor rodando na porta 3000\n Abra http://localhost:3000");
