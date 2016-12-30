32c3ee3b38be245ed9c1fa1935b48b5a70984e14
Heloá Meneses <heloa.meneses@gmail.com>
2016-12-30 18:23:43 -0200

    Removing unused requests

--- server.js
+++ server.js
@@ -3,14 +3,5 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.get('/users', function (req, res) {
-    console.log("I received a GET request");
-});
-
-app.post('/users', function (req, res) {
-    console.log(res.body + ' OII');
-
-});
-
 app.listen(process.env.PORT || 5000)
 console.log("Servidor rodando na porta 5000\nAbra http://localhost:5000");
